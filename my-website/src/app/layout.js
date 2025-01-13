af98047fff89bf23d05f61635e4ef979b780b8ba
akarduna <akarduna@colostate.edu>
2025-01-13 14:10:32 -0800

    started learning next

--- my-website/src/app/layout.js
+++ my-website/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { inter } from "../../ui/fonts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         {children}
       </body>
